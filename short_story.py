9b97c91015dd88e5dd6309b3683c136166a01b12
Dagim Fikru <dagimfikru@gmail.com>
2022-06-01 03:50:53 +0300

    add sphere

--- short_story.py
+++ short_story.py
@@ -13,10 +13,11 @@ from OpenGL.GLU import *
 import numpy as np
 
 edges = ((0, 1), (0, 3), (0, 4),
-         (2, 1), (2, 3), (2, 7),
-         (6, 3), (6, 4),
-         (6, 7), (5, 1),
-         (5, 4), (5, 7))
+        (2, 1), (2, 3), (2, 7),
+        (6, 3), (6, 4),
+        (6, 7), (5, 1),
+        (5, 4), (5, 7))
+
 
 vertices = ((1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, -1),
             (1, -1, 1), (1, 1, 1), (-1, -1, 1), (-1, 1, 1))
@@ -29,6 +30,15 @@ def Cube():
             glVertex3fv(vertices[vertex])
     glEnd()
 
+def Sphere():
+    sphere = gluNewQuadric() 
+    glMatrixMode(GL_PROJECTION)
+
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
+    viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+    glLoadIdentity()
+    gluSphere(sphere, 1.0, 32,26)
 
 def main():
     pygame.init()
@@ -49,7 +59,8 @@ def main():
         # glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glColor3f(a, b, c)
-        Cube()
+        # Cube()
+        Sphere()
         pygame.display.flip()
         a += 1
         b += 1
